e8a92bb1e3a890ac41636937d182a82181326f04
aausaf Alam <aausaf@c3ihub.iitk.ac.in>
2025-08-18 18:33:53 +0530

    [update]

--- src/pages/R&DPavilion/components/VideoModal/index.jsx
+++ src/pages/R&DPavilion/components/VideoModal/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const VideoModal = () => {
     return (
         <div>
-            <video src="/connect/CCETC24.mp4" controls autoPlay preload="metadata" width="100%" />
+            <video src="/connect/video/connect_video.mp4" controls autoPlay preload="metadata" width="100%" />
         </div>
     );
 };
--- src/pages/R&DPavilion/index.jsx
+++ src/pages/R&DPavilion/index.jsx
@@ -9,136 +9,119 @@ import Modal from "../../components/modal";
 const VideoModal = React.lazy(() => import("./components/VideoModal"));
 
 const RDExhibit = () => {
-  const { type } = useParams();
+    const { type } = useParams();
 
-  useEffect(() => {
-    setData();
-    setTabDefaultState();
-  }, [type]);
+    useEffect(() => {
+        setData();
+        setTabDefaultState();
+    }, [type]);
 
-  const [data, setData] = useState([]);
-  const [modalContent, setModalContent] = useState({});
-  const [showModel, setShowModel] = useState(false);
-  const [showVideoPopup, setVideoPopup] = useState(false);
-  const toggleModel = () => setShowModel(!showModel);
-  const toggleVideoPopup = () => setVideoPopup(!showVideoPopup);
-  const [tab, setTab] = useState(1);
+    const [data, setData] = useState([]);
+    const [modalContent, setModalContent] = useState({});
+    const [showModel, setShowModel] = useState(false);
+    const [showVideoPopup, setVideoPopup] = useState(false);
+    const toggleModel = () => setShowModel(!showModel);
+    const toggleVideoPopup = () => setVideoPopup(!showVideoPopup);
+    const [tab, setTab] = useState(1);
 
-  const setTabDefaultState = () => {
-    switch (type) {
-      case "exhibition":
-        setTab(1);
-        break;
-      case "product-launch":
-        setTab(2);
-        break;
-      case "startup-pitching":
-        setTab(3);
-        break;
-      default:
-        setTab(1);
-        break;
-    }
-  };
-
-  return (
-    <section className={styles.container_wrapper}>
-     
-      <div className={styles.container_sub_title}>
-        Explore how start-ups nurtured at c3ihub are shaping the future of
-        cybersecurity. Join us for Live Demonstrations, Product Launch, and
-        Pitching Session that redefine digital safety
-      </div>
-      <button className={styles.playBtn}></button>
-      {/* <button onClick={toggleVideoPopup} className={styles.playBtn}></button> */}
-
-
-      <div className={styles.tab}>
-        <p onClick={() => setTab(1)} className={tab === 1 && styles.active}>
-          Exhibition
-        </p>
-        <p onClick={() => setTab(2)} className={tab === 2 && styles.active}>
-          Product Launch
-        </p>
-        <p onClick={() => setTab(3)} className={tab === 3 && styles.active}>
-          Startup Pitching
-        </p>
-      </div>
-
-      {tab === 1 &&
-        exhibition.map((section, index) => (
-          <div key={index}>
-            <h3 className={styles.section_name}>{section.name}</h3>
-            <div className={`${styles.grid} ${styles.startup_grid}`}>
-              {section.data.map((item, index) => (
-                <div className={styles.grid_item} key={index}>
-                  <a href={item.url} target="_blank" rel="noopener noreferrer">
-                    <img
-                      className={styles[`logo_${index + 1}`]}
-                      src={`${require(`./images/${section.basePath}/${item.logo}`)}`}
-                      alt={item.logo}
-                    />
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-
-      {tab === 2 &&
-        productLaunch.map((section, index) => (
-          <div key={index}>
-            <h3 className={styles.section_name}>{section.name}</h3>
-            <div className={`${styles.grid} ${styles.startup_grid}`}>
-              {section.data.map((item, index) => (
-                <div className={styles.grid_item} key={index}>
-                  <a href={item.url} target="_blank" rel="noopener noreferrer">
-                    <img
-                      className={styles[`logo_${index + 1}`]}
-                      src={`${require(`./images/${section.basePath}/${item.logo}`)}`}
-                      alt={item.logo}
-                    />
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-
-      {tab === 3 &&
-        startupPitching.map((section, index) => (
-          <div key={index}>
-            <h3 className={styles.section_name}>{section.name}</h3>
-            <div className={`${styles.grid} ${styles.startup_grid}`}>
-              {section.data.map((item, index) => (
-                <div className={styles.grid_item} key={index}>
-                  <a href={item.url} target="_blank" rel="noopener noreferrer">
-                    <img
-                      className={styles[`logo_${index + 1}`]}
-                      src={`${require(`./images/${section.basePath}/${item.logo}`)}`}
-                      alt={item.logo}
-                    />
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-
-      {showModel && <MoreInfoModal data={modalContent} onClose={toggleModel} />}
-      <Modal
-        state={showVideoPopup}
-        title={"CCETC Startup video"}
-        setState={toggleVideoPopup}
-        style={{ width: "95%" }}
-        bodyContent={
-          <Suspense fallback={<div>Loading video...</div>}>
-            <VideoModal />
-          </Suspense>
+    const setTabDefaultState = () => {
+        switch (type) {
+            case "exhibition":
+                setTab(1);
+                break;
+            case "product-launch":
+                setTab(2);
+                break;
+            case "startup-pitching":
+                setTab(3);
+                break;
+            default:
+                setTab(1);
+                break;
         }
-      />
-    </section>
-  );
+    };
+
+    return (
+        <section className={styles.container_wrapper}>
+            <div className={styles.container_sub_title}>
+                Explore how start-ups nurtured at c3ihub are shaping the future of cybersecurity. Join us for Live Demonstrations, Product Launch, and Pitching Session that redefine digital safety
+            </div>
+            <button onClick={toggleVideoPopup} className={styles.playBtn}></button>
+
+            <div className={styles.tab}>
+                <p onClick={() => setTab(1)} className={tab === 1 && styles.active}>
+                    Exhibition
+                </p>
+                <p onClick={() => setTab(2)} className={tab === 2 && styles.active}>
+                    Product Launch
+                </p>
+                <p onClick={() => setTab(3)} className={tab === 3 && styles.active}>
+                    Startup Pitching
+                </p>
+            </div>
+
+            {tab === 1 &&
+                exhibition.map((section, index) => (
+                    <div key={index}>
+                        <h3 className={styles.section_name}>{section.name}</h3>
+                        <div className={`${styles.grid} ${styles.startup_grid}`}>
+                            {section.data.map((item, index) => (
+                                <div className={styles.grid_item} key={index}>
+                                    <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                        <img className={styles[`logo_${index + 1}`]} src={`${require(`./images/${section.basePath}/${item.logo}`)}`} alt={item.logo} />
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+
+            {tab === 2 &&
+                productLaunch.map((section, index) => (
+                    <div key={index}>
+                        <h3 className={styles.section_name}>{section.name}</h3>
+                        <div className={`${styles.grid} ${styles.startup_grid}`}>
+                            {section.data.map((item, index) => (
+                                <div className={styles.grid_item} key={index}>
+                                    <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                        <img className={styles[`logo_${index + 1}`]} src={`${require(`./images/${section.basePath}/${item.logo}`)}`} alt={item.logo} />
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+
+            {tab === 3 &&
+                startupPitching.map((section, index) => (
+                    <div key={index}>
+                        <h3 className={styles.section_name}>{section.name}</h3>
+                        <div className={`${styles.grid} ${styles.startup_grid}`}>
+                            {section.data.map((item, index) => (
+                                <div className={styles.grid_item} key={index}>
+                                    <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                        <img className={styles[`logo_${index + 1}`]} src={`${require(`./images/${section.basePath}/${item.logo}`)}`} alt={item.logo} />
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+
+            {showModel && <MoreInfoModal data={modalContent} onClose={toggleModel} />}
+            <Modal
+                state={showVideoPopup}
+                title={"CCETC Startup video"}
+                setState={toggleVideoPopup}
+                style={{ width: "95%" }}
+                bodyContent={
+                    <Suspense fallback={<div>Loading video...</div>}>
+                        <VideoModal />
+                    </Suspense>
+                }
+            />
+        </section>
+    );
 };
 
 export default RDExhibit;
